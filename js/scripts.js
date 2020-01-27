26aa120c4f9b57fdf29efea94b737bcf2bee0e52
KataVuorinen <59822390+KataVuorinen@users.noreply.github.com>
2020-01-27 14:39:57 +0200

    Updated the getNext and getPrve functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,62 @@
-(function () {
-    var itemClassName = 'slider__photo';
-        items = document.getElementsByClassName(itemClassName),
-        totalItems = items.length,
-        slide = 0;
+(function() {
+  var itemClassName = "slider__photo";
+  (items = document.getElementsByClassName(itemClassName)),
+    (totalItems = items.length),
+    (slide = 0);
 
-    // Set event listeners
-    function setEventListeners() {
-        var next = document.getElementsByClassName('slider__button--next')[0],
-            prev = document.getElementsByClassName('slider__button--prev')[0];
+  // Set event listeners
+  function setEventListeners() {
+    var next = document.getElementsByClassName("slider__button--next")[0],
+      prev = document.getElementsByClassName("slider__button--prev")[0];
 
-        next.addEventListener('click', getNext);
-        prev.addEventListener('click', getPrev);
+    next.addEventListener("click", getNext);
+    prev.addEventListener("click", getPrev);
+  }
+
+  function getNext() {
+    if (slide < 4) {
+      items[slide].classList.remove("active");
+      slide++;
+      items[slide].classList.add("active");
+    } else {
+      items[slide].classList.remove("active");
+      slide = 0;
+      items[slide].classList.add("active");
     }
 
-    function getNext() {
-        // TODO
+    // TODO
+  }
+  //This is the elegant way of doing the  getNext function.
+  // function getNext(){
+  //   items[slide].classList.remove("active");
+  // if (slide === totalItems -1) {
+  //    slide = 0;
+  //} else {
+  // slide++;
+  //}
+  //  items[slide].classList.add("active");
+  //}
+  function getPrev() {
+    if (slide > 0) {
+      items[slide].classList.remove("active");
+      slide--;
+      items[slide].classList.add("active");
+    } else {
+      items[slide].classList.remove("active");
+      slide = totalItems - 1;
+      items[slide].classList.add("active");
     }
 
-    function getPrev(){
-        // TODO      
-    }
+    // TODO
+  }
 
-    function initSlider() {
-        // setInitialClasses();
-        setEventListeners();
-        
-        // Set moving to false so that the slider becomes interactive
-        moving = false;
-    }
+  function initSlider() {
+    // setInitialClasses();
+    setEventListeners();
 
-    initSlider();
+    // Set moving to false so that the slider becomes interactive
+    moving = false;
+  }
+
+  initSlider();
 })();
