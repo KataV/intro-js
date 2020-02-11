120ccf20b628f39c5df95b2bdcbeaf57b1ffaf41
KataVuorinen <59822390+KataVuorinen@users.noreply.github.com>
2020-02-11 14:30:43 +0200

    Updated code to search pictures

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,69 @@
-var flickrAPI = 'add-the-URL-here';
+var flickrAPI = "add-the-URL-here";
 
 /**
  * Call the render function to render image content if there are result,
  * otherwise, discard and print out the notification that nothing is found
  * Call addFavouriteButtonEvent function once finished rendering images
- * 
+ *
  * @param   {Object}    resp contains the data that we retrieved from the AJAX call to to flickr
  * @return  {Boolean}   false if no image is found
  * @return  {Void}      if images get rendered
  */
 function handleResponse(resp) {
-    console.log('The request has been completed'); 
-    console.log(resp);
+  console.log("The request has been completed");
+  console.log(resp);
 
-    if (resp.items.length === 0) {
-        $('#imageWrapper').text('There is no image matching your search term');
-        return;
-    }
+  if (resp.items.length === 0) {
+    $("#imageWrapper").text("There is no image matching your search term");
+    return;
+  }
 
-    renderContent(resp);
+  renderContent(resp);
 }
 
 function renderContent(resp) {
-    // TODO add render logic here
-    var _newImageEl = createImage(url);
-    $('.imageWrapper').append(newImageEl);
+  // TODO add render logic here
+  var _newImageEl = createImage(url);
+  $(".imageWrapper").append(newImageEl);
 }
 
 function createImage(url) {
-    // TODO generate image element here
+  // TODO generate image element here
 }
 
 function doAjaxCall(searchTerm) {
-    var paramsForFlickr = {
-        tags: searchTerm,
-        tagmode: 'any',
-        format: 'json'
-    };
+  var paramsForFlickr = {
+    tags: searchTerm,
+    tagmode: "any",
+    format: "json"
+  };
 
-    $.getJSON( 
-        flickrAPI, 
-        paramsForFlickr,
-        handleResponse
-    );
+  $.getJSON(flickrAPI, paramsForFlickr, handleResponse);
 }
+
+/*function clickAlert() {
+  alert("You searched: " + document.searchBar.searchQuery.value);
+}
+*/
+
+$(document).ready(function() {
+  $('[name="searchButton"]').click(function() {
+    alert("Searched: " + document.searchBar.searchQuery.value);
+
+    $.ajax({
+      url: "//api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+      dataType: "json",
+      data: {
+        tags: document.searchBar.searchQuery.value,
+        tagmode: "any",
+        format: "json"
+      }
+    }).done(function(data) {
+      console.log(data);
+      $("#imageWrapper").empty();
+      data.items.forEach(function(item) {
+        $("#imageWrapper").append("<img src=" + item.media.m + "></img");
+      });
+    });
+  });
+});
